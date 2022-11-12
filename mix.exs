270824eb5f5c9d535c2fa5ca77b5f786f3567478
Jimmy Bosse <jimmy@jobostudios.net>
2022-11-12 15:59:13 -0500

    Adding mix test.watch

--- mix.exs
+++ mix.exs
@@ -51,7 +51,8 @@ defmodule Pelican.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
